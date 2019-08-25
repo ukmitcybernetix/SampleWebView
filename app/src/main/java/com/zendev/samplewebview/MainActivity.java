55ffa5bfa224bdcba27fff21af11d39cdeabbef8
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-08-25 22:47:17 +0700

    Set JavaScript Enabled

--- app/src/main/java/com/zendev/samplewebview/MainActivity.java
+++ app/src/main/java/com/zendev/samplewebview/MainActivity.java
@@ -2,11 +2,13 @@ package com.zendev.samplewebview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
         WebView webView = findViewById(R.id.webView);
         webView.loadUrl("http://sisfo.upiyptk.ac.id");
+        webView.getSettings().setJavaScriptEnabled(true);
     }
 }
