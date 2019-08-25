164533f061c677b841d2f2b7e0cea2e1c8333d71
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-08-25 23:31:44 +0700

    Update Main Activity Class and Add ProgressBar

--- app/src/main/java/com/zendev/samplewebview/MainActivity.java
+++ app/src/main/java/com/zendev/samplewebview/MainActivity.java
@@ -3,8 +3,15 @@ package com.zendev.samplewebview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +22,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView = findViewById(R.id.webView);
-        webView.loadUrl("http://sisfo.upiyptk.ac.id");
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                view.loadUrl("javascript:alert('Website Successfully Loaded')");
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                result.confirm();
+                return true;
+            }
+        });
+
+        webView.loadUrl("http://www.if-upiyptk.info/");
     }
 }
