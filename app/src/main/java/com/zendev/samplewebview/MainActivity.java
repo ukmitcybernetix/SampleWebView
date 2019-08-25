5f7d871eea4da6179a4d2448eff584cf0f900d5e
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-08-25 22:43:37 +0700

    Update Main Activity Class and Add Internet Permission

--- app/src/main/java/com/zendev/samplewebview/MainActivity.java
+++ app/src/main/java/com/zendev/samplewebview/MainActivity.java
@@ -3,6 +3,7 @@ package com.zendev.samplewebview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.loadUrl("http://sisfo.upiyptk.ac.id");
     }
 }
